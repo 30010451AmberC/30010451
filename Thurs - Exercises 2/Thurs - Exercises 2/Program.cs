6112e30fe562403fdfe3ee0f4857da2043631503
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-02-28 12:59:13 +1300

    Thurs exercise not complete

---
+++ Thurs - Exercises 2/Thurs - Exercises 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thurs___Exercises_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercises 2
+            //Task 1
+            //Asking user for a number and creating a new array the size of the number entered by the user. 
+            Console.WriteLine("Please enter a number for the size of the array");
+            int[] num = new int[int.Parse(Console.ReadLine())];
+
+            //Creating a new random number genterator.
+            Random rand = new Random();
+
+            //Loop to fill array with random numbers
+            for (int i = 0; i <num.Length; i++)
+            {
+                num[i] = rand.Next(1,100);
+            }
+
+            //Display array in ascending order to console
+            //Console.WriteLine(num.Sort); ??
+
+
+        }
+    }
+}
